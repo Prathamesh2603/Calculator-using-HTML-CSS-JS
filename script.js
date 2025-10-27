e00ca8a6540ae8be33196efb23f9731ba9eacd9e
Prathamesh2603 <prathameshpawar645@gmail.com>
2025-10-27 15:26:42 +0530

    javascript code for calculator is done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const inputBox = document.querySelector("#inputBox");
+const buttons = document.querySelectorAll("button");
 
+let string = "";
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.innerText === "=") {
+            string = eval(string);
+            inputBox.value = string
+        } else if (button.innerText === "AC") {
+            string = "";
+            inputBox.value = string;
+        } else if (button.innerText === "DEL") {
+            string = string.substring(0, string.length-1);
+            inputBox.value = string;
+        } else {
+            string += button.innerText;
+            inputBox.value = string;
+        }
+    });
+});
